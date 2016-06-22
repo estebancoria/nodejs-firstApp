61f45af7fbdab3c54b45278225427c1e5345def1
Esteban Coria <esteban.coria@despegar.com>
2016-06-22 13:49:28 -0300

    Using jade to create the HTML template

--- hola_express/app.js
+++ hola_express/app.js
@@ -2,8 +2,10 @@ var express = require("express");
 
 var app = express();
 
+app.set("view engine", "jade");
+
 app.get("/", function (req, res) {
-    res.send("Hola Mundo");
+    res.render("index",{variable:"Hola Jipi"});
 });
 
 app.listen(8083);
